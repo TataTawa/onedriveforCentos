e17496c060c35c3e6c17d22a25ef1bd1df8ecbdd
TataTawa <19670131+TataTawa@users.noreply.github.com>
2022-04-26 20:33:06 +0800

    Create flinkrl.sh

---
+++ flinkrl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo su
+nohup python flinkrl.py &
+nohup python flinkrl.py &
+nohup python flinkrl.py &
+nohup python flinkrl.py &
+nohup python flinkrl.py &
+nohup python flinkrl.py &
+nohup python flinkrl.py &
+nohup python flinkrl.py &
